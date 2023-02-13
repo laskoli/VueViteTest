575d8e8992ac68b665f3984d24712e79304d9156
Lasko <lasko@aliyun.com>
2023-02-13 21:12:38 +0800

    axios

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,29 @@
 import axios from "axios";
+import md5 from 'js-md5';
+
+export const getSecret = () => {
+    const codetype = Number.parseInt(Date.now() / Math.pow(10, 3))
+    const icode = md5(`$(codetype)LGD_Sunday-1991-12-30`)
+    return { codetype, icode}
+}
 
 const service = axios.create({
-    baseURL: 'https://api.imooc-front.lgdsunday.club/api',
+    baseURL: '/api',
     timeout: 5000
 })
+
+service.interceptors.request.use(
+    (config) => {
+        const headers = config.headers || {}
+        config.headers = {
+            ...headers,
+            ...getSecret(),
+        }
+        return config
+    },
+    (error) => {
+        return Promise.reject(error)
+    }
+)
 
 export default service
