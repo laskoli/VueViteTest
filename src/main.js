eb5158c4df552dd4c3c0eee1811a79eef05d0608
Lasko <lasko@uupz.cn>
2023-02-14 01:21:06 +0800

    change tailwind
    TODO: Justify tailwind fontsize

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router  from './routers'
+import { useREM } from './utils/flexible'
 import './styles/index.scss'
 
+useREM()
 createApp(App).use(router).mount('#app')
--- src/utils/flexible.js
+++ src/utils/flexible.js
@@ -10,3 +10,20 @@ const { width } = useWindowSize()
 export const isMobileTerminal = computed(() => {
     return width.value < PC_DEVICE_WIDTH
 })
+
+/**
+ *  动态指定 rem  基准值， 最大为 40px 
+ *  根据用户屏幕宽度， 进行计算 ，把计算值赋值 html fontsize 大小
+*/
+export const useREM = () => {
+    // 定义最大的 fontSize
+    const MAX_FONT_SIZE = 40
+    // 监听html 文档初解析完成的事件
+    document.addEventListener('DOMContentLoaded', () => {
+        const html = document.querySelector('html')
+        // 根据屏幕宽度/ 10 
+        let fontSize = window.innerWidth / 10 
+        fontSize = fontSize > MAX_FONT_SIZE ? MAX_FONT_SIZE : fontSize
+        html.style.fontSize = fontSize + 'px'
+    })
+}
