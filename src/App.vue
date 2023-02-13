8f70c6f2d3aeaa816b5fe00b9e05db2f3d3b22c3
Lasko <lasko@aliyun.com>
2023-02-13 15:11:23 +0800

    add  routes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>{{ isMobileTerminal }}</div>
+  <!-- 一级路由出口 -->
+  <router-view/>
 </template>
 
 <script setup>
-import { isMobileTerminal } from './utils/flexible';
 </script>
 
 <style lang="scss" scoped></style>
