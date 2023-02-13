84d45a202162d39399851cbf97cc7fbd55a5b212
Lasko <lasko@aliyun.com>
2023-02-13 14:45:59 +0800

    use vueuse

--- src/utils/flexible.js
+++ src/utils/flexible.js
@@ -1,8 +1,12 @@
 import { computed } from 'vue'
 import { PC_DEVICE_WIDTH } from "../constants"
+import { useWindowSize } from '@vueuse/core'
+
+const { width } = useWindowSize()
+
 /**
  * 判断当前是否为移动设备， 判断依据是屏幕宽度是否小于一个批定宽度（1280—）
  */
 export const isMobileTerminal = computed(() => {
-    return document.documentElement.clientWidth < PC_DEVICE_WIDTH
+    return width.value < PC_DEVICE_WIDTH
 })
