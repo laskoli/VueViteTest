68e554b9b1e4ecc4b0f8dc55adfa8f48aa3a2727
Lasko <lasko@uupz.cn>
2023-02-16 22:01:08 +0800

    register svg icons

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import router  from './routers'
 import { useREM } from './utils/flexible'
 import myLibs from './libs'
 import './styles/index.scss'
-
+// 注册 svg-icons
+import 'virtual:svg-icons-register'
 
 useREM()
 createApp(App).use(router).use(myLibs).mount('#app')
