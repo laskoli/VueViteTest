e25db722158a88d27025c238bfe96f4f130ebd90
Lasko <lasko@uupz.cn>
2023-02-16 23:22:17 +0800

    slider style

--- src/views/main/components/navigation/mobile/index.vue
+++ src/views/main/components/navigation/mobile/index.vue
@@ -1,26 +1,93 @@
 <template>
-    <div class="bg-white sticky top-0 left-0 z-10">
-       <ul class=" relative flex overflow-x-auto p-1 text-xs text-zinc-600 overflow-hidden">
-            <!-- 汉堡按钮 -->
-            <li class=" fixed top-0 right-[-1px] h-4 px-1 flex items-center bg-white z-20 shadow-l-white">
-                <m-svg-icon class="w-1.5 h-1.5" name="hamburger"></m-svg-icon>
-            </li>
-            <li v-for="item in data" :key="item.id" class=" shrink-0 px-1.5 py-0.5 z-10 duration-200">{{ item.name }}</li>
-       </ul> 
-    
-    </div>
+  <div class="bg-white sticky top-0 left-0 z-10">
+    <ul
+      ref="ulTarget"
+      class="relative flex overflow-x-auto p-1 text-xs text-zinc-600 overflow-hidden"
+    >
+      <!-- 滑块 -->
+      <li
+        ref="sliderTarget"
+        :style="sliderStyle"
+        class="absolute h-[22px] bg-zinc-900 rounded-lg duration-200"
+      ></li>
+      <!-- 汉堡按钮 -->
+      <li
+        class="fixed top-0 right-[-1px] h-4 px-1 flex items-center bg-white z-20 shadow-l-white"
+      >
+        <m-svg-icon class="w-1.5 h-1.5" name="hamburger"></m-svg-icon>
+      </li>
+      <!-- items -->
+      <li
+        v-for="(item, index) in data"
+        :key="item.id"
+        class="shrink-0 px-1.5 py-0.5 z-10 duration-200 last:mr-4"
+        :class="{
+            'text-zinc-100': currentCategoryIndex === index
+        }"
+        :ref="setItemRef"
+        @click="onItemClick(index)"
+      >
+        {{ item.name }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup>
+import { onBeforeUpdate, ref, watch } from 'vue'
+import { useScroll } from '@vueuse/core'
+// import { watch } from 'fs'
+
 // vite 构建 项目中，可以使用 defineProps方法
 defineProps({
-    data: {
-        type: Array,
-        required: true
-    }
+  data: {
+    type: Array,
+    required: true
+  }
 })
+
+// 滑块
+const sliderStyle = ref({
+  transform: 'translateX(0px)',
+  width: '60px'
+})
+
+// 选中 item 下标
+const currentCategoryIndex = ref(0)
+
+// 获取所有的 Item 元素
+let itemRefs = []
+const setItemRef = (el) => {
+  if (el) {
+    itemRefs.push(el)
+  }
+  //console.log(el)
+}
+// 数据改变之后 ，Dom改变之前
+onBeforeUpdate(() => {
+  itemRefs = []
+})
+
+// 获取 ul 元素
+const ulTarget = ref(null)
+// 能过 vueuse -> useScroll 获取响应式的 scroll 滚动距离
+const { x: ulScrollLeft } = useScroll(ulTarget)
+
+// watch 监听
+watch(currentCategoryIndex, (val) => {
+  const { left, width } = itemRefs[val].getBoundingClientRect()
+  //console.log(rect)
+  sliderStyle.value = {
+    //滑块的位置 = ul 横向滚动的位置 + 当前元素的 left - ul 的 padding
+    transform: `translateX(${ulScrollLeft.value + left - 10}px)`,  
+    width: width + 'px'
+  }
+})
+
+// item 点击事件
+const onItemClick = (index) => {
+  currentCategoryIndex.value = index
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
